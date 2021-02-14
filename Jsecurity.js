1bc5256888e1655c03218c190acd169938371523
ewtv <78833551+ewtv@users.noreply.github.com>
2021-02-14 16:34:04 +0900

    Create Jsecurity.js

---
+++ Jsecurity.js
@@ -0,0 +1,99 @@
+var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
+var sentence = newURL;
+var start = sentence.indexOf("");
+var end = sentence.indexOf(":", start);
+
+var list = sentence.substring(start, end);   
+if(list=='http') {
+alert('🚫이 사이트는 보안 연결(https)가 적용되지 않았습니다! 공격자(해커)의 공격으로 개인 정보가 유출될수 있으니 중요한 정보는 적지 마세요.');
+const arr = ['https://blow.ga//vitamin303','https://blow.ga//vitamin303/','https://playentry.org//ds#!/free/5f6336272dea1a00e6cffb42?category=free&rows=20&title=&page=1&currentPage=1&sort=created&isOwnerView=false&search_title=','https://blow.ga//울프나쁨','https://blow.ga//울프나쁨/','https://www.fake.r-e.kr//','https://www.fake.r-e.kr','http://naver.me//5xNPYqk3','http://naver.me//5xNPYqk3/','https://qops.xyz//RGmLjE','https://blow.ga//20725','https://url.kr//AeMji8','https://blow.ga//vitaminface','https://www.itemmania.com','https://itemmania.com','https://qops.xyz//pgcIe7','https://blow.ga//sd','https://youtu.be//N_I2O1RBS38','https://www.naver.com//'];
+console.log('✅사이트 검사를 시작합니다');
+let m1 = newURL
+for(let index in arr){
+if((arr[index])==window.location) {if(index >1000){alert("안전한 사이트입니다.")}else{document.write('<h1 style="color:red">'+"🚫해당 사이트는 신고된 악성 사이트입니다"+"</h1>"+"<hr>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>");}};
+};let WARNING = false;
+arr.forEach(e => {
+WARNING = WARNING || e == m1
+})
+if(WARNING) document.write('<h1 style="color:red">'+"🚫해당 사이트는 신고된 악성 사이트입니다"+"</h1>"+"<hr>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>");
+else {
+console.log('✅멜웨어 검사를 시작합니다');
+var sentence = document.documentElement.innerHTML;
+var start = sentence.indexOf("<a href=");
+var end = sentence.indexOf("</a>", start);
+if(start=="'Ben.exe'"){
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<hr>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>");
+}else if(start=='"Ben.exe"'){
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<hr>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>");
+}else{
+alert('✅검사 완료 해당 사이트에서 맬웨어가 발견되지 않았으며 해당 사이트가 신고되지도 않았습니다.. 하지만, 새로운 위험 사이트 일수 있으니 조심하시길 바랍니다');location.reload();};
+}
+}
+else if(list=="https"){
+alert('✅이 사이트는 보안 연결(https)가 적용되었습니다! 공격자(해커)의 공격으로도 개인정보가 쉽게 유출되지 않습니다.');
+const arr = ['https://blow.ga//vitamin303','https://blow.ga//vitamin303/','https://playentry.org//ds#!/free/5f6336272dea1a00e6cffb42?category=free&rows=20&title=&page=1&currentPage=1&sort=created&isOwnerView=false&search_title=','https://blow.ga//울프나쁨','https://blow.ga//울프나쁨/','https://www.fake.r-e.kr//','https://www.fake.r-e.kr','http://naver.me//5xNPYqk3','http://naver.me//5xNPYqk3/','https://qops.xyz//RGmLjE','https://blow.ga//20725','https://url.kr//AeMji8','https://blow.ga//vitaminface','https://www.itemmania.com','https://itemmania.com','https://qops.xyz//pgcIe7','https://blow.ga//sd','https://youtu.be//N_I2O1RBS38','https://www.naver.com//'];
+console.log('✅사이트 검사를 시작합니다');
+let m1 = newURL
+for(let index in arr){
+if((arr[index])==window.location) {if(index >1000){alert("안전한 사이트입니다.")}else{document.write('<h1 style="color:red">'+"🚫해당 사이트는 신고된 악성 사이트입니다"+"</h1>"+"<hr>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>");}};
+};let WARNING = false;
+arr.forEach(e => {
+WARNING = WARNING || e == m1
+})
+if(WARNING) document.write('<h1 style="color:red">'+"🚫해당 사이트는 신고된 악성 사이트입니다"+"</h1>"+"<hr>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>");
+else {
+console.log('✅멜웨어 검사를 시작합니다');
+var sentence = document.documentElement.innerHTML;
+var start = sentence.indexOf("<a href=");
+var end = sentence.indexOf("</a>", start);
+if(start=="'Ben.exe'"){
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<hr>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>");
+}else if(start=='"Ben.exe"'){
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<hr>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>");
+}else{
+alert('✅검사 완료 해당 사이트에서 맬웨어가 발견되지 않았으며 해당 사이트가 신고되지도 않았습니다.. 하지만, 새로운 위험 사이트 일수 있으니 조심하시길 바랍니다');location.reload();};
+}
+}
+else if(list=="chrome"){
+alert('✅✅매우 안전한 크롬의 페이지입니다. 어떤 정보든 믿고 입력하셔도 됩니다.');
+alert('안전한 페이지이므로 사이트 검사를 진행하지 않습니다.');
+}
+else if(list=="edge"){
+alert('✅✅매우 안전한 엣지의 페이지입니다');
+alert('안전한 페이지이므로 사이트 검사를 진행하지 않습니다.');
+}
+else if(list=="whale"){
+alert('✅✅매우 안전한 웨일의 페이지입니다. 어떤 정보든 믿고 입력하셔도 됩니다.');
+alert('안전한 페이지이므로 사이트 검사를 진행하지 않습니다.');
+}
+else if(list=="opera"){
+alert('✅✅매우 안전한 오페라의 페이지입니다. 어떤 정보든 믿고 입력하셔도 됩니다.');
+alert('안전한 페이지이므로 사이트 검사를 진행하지 않습니다.');
+}else{
+alert('❔죄송하지만, 이 페이지의 정보를 알수 없습니다. 페이지 속성 : '+list);
+alert('알수 없는 페이지이므로 사이트 검사를 진행합니다.');
+const arr = ['https://blow.ga//vitamin303','https://blow.ga//vitamin303/','https://playentry.org//ds#!/free/5f6336272dea1a00e6cffb42?category=free&rows=20&title=&page=1&currentPage=1&sort=created&isOwnerView=false&search_title=','https://blow.ga//울프나쁨','https://blow.ga//울프나쁨/','https://www.fake.r-e.kr//','https://www.fake.r-e.kr','http://naver.me//5xNPYqk3','http://naver.me//5xNPYqk3/','https://qops.xyz//RGmLjE','https://blow.ga//20725','https://url.kr//AeMji8','https://blow.ga//vitaminface','https://www.itemmania.com','https://itemmania.com','https://qops.xyz//pgcIe7','https://blow.ga//sd','https://youtu.be//N_I2O1RBS38','https://www.naver.com//'];
+console.log('✅사이트 검사를 시작합니다');
+let m1 = newURL
+for(let index in arr){
+if((arr[index])==window.location) {if(index >1000){alert("안전한 사이트입니다.")}else{document.write('<h1 style="color:red">'+"🚫해당 사이트는 신고된 악성 사이트입니다"+"</h1>"+"<hr>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>");}};
+};let WARNING = false;
+arr.forEach(e => {
+WARNING = WARNING || e == m1
+})
+if(WARNING) document.write('<h1 style="color:red">'+"🚫해당 사이트는 신고된 악성 사이트입니다"+"</h1>"+"<hr>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>");
+else {
+console.log('✅멜웨어 검사를 시작합니다');
+var sentence = document.documentElement.innerHTML;
+var start = sentence.indexOf("<a href=");
+var end = sentence.indexOf("</a>", start);
+if(start=="'Ben.exe'"){
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<hr>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>");
+}else if(start=='"Ben.exe"'){
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<hr>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>");
+}else{
+alert('✅검사 완료 해당 사이트에서 맬웨어가 발견되지 않았으며 해당 사이트가 신고되지도 않았습니다.. 하지만, 새로운 위험 사이트 일수 있으니 조심하시길 바랍니다');location.reload();};
+}
+setTimeout(function() {
+  location.reload();
+}, 300000);};
