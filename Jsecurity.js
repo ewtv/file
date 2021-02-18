bd58a2d39acbbec157d8aba20bab375883982020
ewtv <78833551+ewtv@users.noreply.github.com>
2021-02-19 00:16:20 +0900

    Update Jsecurity.js

--- Jsecurity.js
+++ Jsecurity.js
@@ -87,7 +87,6 @@ alert('✅✅매우 안전한 오페라의 페이지입니다. 어떤 정보든 
 alert('안전한 페이지이므로 사이트 검사를 진행하지 않습니다.');
 }else{
 alert('❔죄송하지만, 이 페이지의 정보를 알수 없습니다. 페이지 속성 : '+list+' 잠시후 맬웨어 검사가 실행됩니다.');
-alert('알수 없는 페이지이므로 사이트 검사를 진행합니다.');
 const arr = ['https://blow.ga//vitamin303','https://blow.ga//vitamin303/','https://playentry.org//ds#!/free/5f6336272dea1a00e6cffb42?category=free&rows=20&title=&page=1&currentPage=1&sort=created&isOwnerView=false&search_title=','https://blow.ga//울프나쁨','https://blow.ga//울프나쁨/','https://www.fake.r-e.kr//','https://www.fake.r-e.kr','http://naver.me//5xNPYqk3','http://naver.me//5xNPYqk3/','https://qops.xyz//RGmLjE','https://blow.ga//20725','https://url.kr//AeMji8','https://blow.ga//vitaminface','https://www.itemmania.com','https://itemmania.com','https://qops.xyz//pgcIe7','https://blow.ga//sd','https://youtu.be//N_I2O1RBS38'];
 console.log('✅사이트 검사를 시작합니다');
 let m1 = newURL
