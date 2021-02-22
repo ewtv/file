4b6f1b7680a6871e6003a944e5cdceb1623c495f
ewtv <78833551+ewtv@users.noreply.github.com>
2021-02-22 15:26:27 +0900

    Update Jsecurity.js

--- Jsecurity.js
+++ Jsecurity.js
@@ -29,14 +29,20 @@ var end = sentence.indexOf(">", start+9);
 
 var list = sentence.substring(start+9, end);  
 if(list=="'Ben.exe'"){
-document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Ben.exe(위험)"+"</p>"+"<br>"+"<p style='color:red>"+"인증서 : 🚫인증서가 없음"+"</p>");
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Ben.exe(위험)"+"</p>"+"<p style='color:red'>"+"인증서 : 🚫인증서가 없음"+"</p>");
 }else if(list=='"Ben.exe"'){
-document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Ben.exe(위험)"+"</p>"+"<br>"+"<p style='color:red>"+"인증서 : 🚫인증서가 없음"+"</p>");
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Ben.exe(위험)"+"</p>"+"<p style='color:red'>"+"인증서 : 🚫인증서가 없음"+"</p>");
 }else if(list=='"Spark.exe"'){
-document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Spark.exe(매우위험)"+"</p>"+"<br>"+"<p style='color:red>"+"인증서 : 🚫인증서가 없음"+"</p>");
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Spark.exe(매우위험)"+"</p>"+"<p style='color:red'>"+"인증서 : 🚫인증서가 없음"+"</p>");
 }
 else if(list=="'Spark.exe'"){
-document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Spark.exe(매우위험)"+"</p>"+"<br>"+"<p style='color:red>"+"인증서 : 🚫인증서가 없음"+"</p>");
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Spark.exe(매우위험)"+"</p>"+"<p style='color:red'>"+"인증서 : 🚫인증서가 없음"+"</p>");
+}
+else if(list=="'000.exe'"){
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : 000.exe(매우위험)"+"</p>"+"<p style='color:red'>"+"인증서 : 🚫인증서가 없음"+"</p>");
+}
+else if(list=='"000.exe"'){
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : 000.exe(매우위험)"+"</p>"+"<p style='color:red'>"+"인증서 : 🚫인증서가 없음"+"</p>");
 }
 else if(start=='-1'){
 alert('✅검사 완료 해당 사이트에서 맬웨어가 발견되지 않았으며 해당 사이트가 신고되지도 않았습니다. 잠시후 보고 결과를 보여드립니다.');alert('보고 결과__인증서 : 🚫ssl인증서가 없음__다운로드 하려는 파일 : 없음__신고여부 : false__사이트 속성 : http__');location.reload();
@@ -65,14 +71,20 @@ var end = sentence.indexOf(">", start+9);
 
 var list = sentence.substring(start+9, end);  
 if(list=="'Ben.exe'"){
-document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Ben.exe(위험)"+"</p>"+"<br>"+"<p style='color:red>"+"인증서 : 🚫인증서가 없음"+"</p>");
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Ben.exe(위험)"+"</p>"+"<p style='color:red'>"+"인증서 : 🚫인증서가 없음"+"</p>");
 }else if(list=='"Ben.exe"'){
-document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Ben.exe(위험)"+"</p>"+"<br>"+"<p style='color:red>"+"인증서 : 🚫인증서가 없음"+"</p>");
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Ben.exe(위험)"+"</p>"+"<p style='color:red'>"+"인증서 : 🚫인증서가 없음"+"</p>");
 }else if(list=='"Spark.exe"'){
-document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Spark.exe(매우위험)"+"</p>"+"<br>"+"<p style='color:red>"+"인증서 : ⚠ssl인증서가 있으나, 인증 업체가 올바르지 않음"+"</p>");
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Spark.exe(매우위험)"+"</p>"+"<p style='color:red'>"+"인증서 : ⚠ssl인증서가 있으나, 인증 업체가 올바르지 않음"+"</p>");
 }
 else if(list=="'Spark.exe'"){
-document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Spark.exe(매우위험)"+"</p>"+"<br>"+"<p style='color:red>"+"인증서 : ⚠ssl인증서가 있으나, 인증 업체가 올바르지 않음"+"</p>");
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Spark.exe(매우위험)"+"</p>"+"<p style='color:red'>"+"인증서 : ⚠ssl인증서가 있으나, 인증 업체가 올바르지 않음"+"</p>");
+}
+else if(list=="'000.exe'"){
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : 000.exe(매우위험)"+"</p>"+"<p style='color:red'>"+"인증서 : ⚠ssl인증서가 있으나, 인증 업체가 올바르지 않음"+"</p>");
+}
+else if(list=='"000.exe"'){
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : 000.exe(매우위험)"+"</p>"+"<p style='color:red'>"+"인증서 : ⚠ssl인증서가 있으나, 인증 업체가 올바르지 않음"+"</p>");
 }
 else if(start=='-1'){
 alert('✅검사 완료 해당 사이트에서 맬웨어가 발견되지 않았으며 해당 사이트가 신고되지도 않았습니다. 잠시후 보고 결과를 보여드립니다.');alert('보고 결과__인증서 : ✅올바른 ssl인증서가 있음__다운로드 하려는 파일 : 없음__신고여부 : false__사이트 속성 : https__');location.reload();
@@ -120,6 +132,21 @@ if(list=="'Ben.exe'"){
 document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Ben.exe(위험)"+"</p>"+"<br>"+"<p style='color:red>"+"인증서 : ❔알수없음(인증서가 올바르지 않음)"+"</p>");
 }else if(list=='"Ben.exe"'){
 document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Ben.exe(위험)"+"</p>"+"<br>"+"<p style='color:red>"+"인증서 : ❔알수없음(인증서가 올바르지 않음)"+"</p>");
+if(list=="'Ben.exe'"){
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Ben.exe(위험)"+"</p>"+"<br>"+"<p style='color:red'>"+"인증서 : 🚫인증서가 없음"+"</p>");
+}else if(list=='"Ben.exe"'){
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Ben.exe(위험)"+"</p>"+"<br>"+"<p style='color:red'>"+"인증서 : 🚫인증서가 없음"+"</p>");
+}else if(list=='"Spark.exe"'){
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Spark.exe(매우위험)"+"</p>"+"<br>"+"<p style='color:red'>"+"인증서 : ❔알수 없음(ssl인증서가 올바르지 않음)"+"</p>");
+}
+else if(list=="'Spark.exe'"){
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Spark.exe(매우위험)"+"</p>"+"<br>"+"<p style='color:red'>"+"인증서 : ❔알수 없음(ssl인증서가 올바르지 않음)"+"</p>");
+}
+else if(list=="'000.exe'"){
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : 000.exe(매우위험)"+"</p>"+"<br>"+"<p style='color:red'>"+"인증서 : ❔알수 없음(ssl인증서가 올바르지 않음)"+"</p>");
+}
+else if(list=='"000.exe"'){
+document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : 000.exe(매우위험)"+"</p>"+"<br>"+"<p style='color:red'>"+"인증서 : ❔알수 없음(ssl인증서가 올바르지 않음)"+"</p>");
 }else if(start=='-1'){
 var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
 var sentence = newURL;
@@ -127,17 +154,6 @@ var start = sentence.indexOf("");
 var end = sentence.indexOf(":", start);
 
 var list = sentence.substring(start+9, end);  
-if(list=="'Ben.exe'"){
-document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Ben.exe(위험)"+"</p>"+"<br>"+"<p style='color:red>"+"인증서 : 🚫인증서가 없음"+"</p>");
-}else if(list=='"Ben.exe"'){
-document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Ben.exe(위험)"+"</p>"+"<br>"+"<p style='color:red>"+"인증서 : 🚫인증서가 없음"+"</p>");
-}else if(list=='"Spark.exe"'){
-document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Spark.exe(매우위험)"+"</p>"+"<br>"+"<p style='color:red>"+"인증서 : ❔알수 없음(ssl인증서가 올바르지 않음)"+"</p>");
-}
-else if(list=="'Spark.exe'"){
-document.write('<h1 style="color:red">'+"🚫해당 사이트에서 바이러스를 발견하였습니다!!"+"</h1>"+"<p style='color:blue'>"+"하지만 안심하세요! 현재 사이트의 모든 코드를 Jsecurity에서 차단하였습니다!"+"</p>"+"<hr>"+"<p style='color:red'>"+"바이러스 정보 : Spark.exe(매우위험)"+"</p>"+"<br>"+"<p style='color:red>"+"인증서 : ❔알수 없음(ssl인증서가 올바르지 않음)"+"</p>");
-}
-else if(start=='-1'){
 alert('✅검사 완료 해당 사이트에서 맬웨어가 발견되지 않았으며 해당 사이트가 신고되지도 않았습니다. 잠시후 보고 결과를 보여드립니다.');alert('보고 결과__인증서 : 🚫ssl인증서가 없음__다운로드 하려는 파일 : 없음__신고여부 : false__사이트 속성 : http__');location.reload();
 }
 else{
